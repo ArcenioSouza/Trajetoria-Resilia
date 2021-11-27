d552daa14de77a97e74e97dbc3a1113446b99563
Arcenio Souza <arcenio_neto@icloud.com>
2021-11-27 10:39:02 -0300

    Adiciona desafio 3 do qualify da semana 5 e comentários em cada desafio

--- qualify-semana5/desafio1/index.js
+++ qualify-semana5/desafio1/index.js
@@ -1,9 +1,14 @@
 function abrevNome(nome){
+  //na variável iniciais primeiro eu pego a primeira letra do nome passado como argumento da função
    let iniciais = nome[0]
+
+   //o for irá percorrer cada caracter do argumento da função e qdo achar um " "(espaço) irá atribuir a varíavel iniciais um "." + a primeira letra do próximo nome passado como argumento.
    for(var i = 0; i < nome.length; i++){
      if(nome[i] == " "){
        iniciais += "."+nome[i + 1]
      }    
    }
+
+   //a função irá retornar os valores colocados na váriável iniciais em letras maiúsculas
    return iniciais.toUpperCase()
  }
