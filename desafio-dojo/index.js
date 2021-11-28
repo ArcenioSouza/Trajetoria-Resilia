a3364e1131902be55ed1ea69757ea22289d6be47
Arcenio Souza <arcenio_neto@icloud.com>
2021-11-28 12:32:19 -0300

    Adiciona resolução do desafio-dojo

--- desafio-dojo/index.js
+++ desafio-dojo/index.js
@@ -1,10 +1,22 @@
 function encontrarPote(array, busca){
+   var valor = false
    for(var i = 0; i < array.length; i++){
       if(array[i] == busca){
-         console.log
+         console.log("Encontrado no pote 0")
+         valor = true
          break
       }
+      for(var j = 0; j < array[i].length; j++){
+         if(array[i][j] == busca){
+            valor = true
+            console.log("Encontrado no pote 1")
+            break
+         }
+      }
    }
-   return potePosition
+   if(valor == false){
+      console.log("Item não encontrado")
+   }
+   
 }
 
