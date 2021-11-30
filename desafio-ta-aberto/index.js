f3a3558871ef35492c473205386ea826123eb499
Arcenio Souza <arcenio_neto@icloud.com>
2021-11-29 22:02:06 -0300

    Adiciona modificações ao desafio ta-aberto

--- desafio-ta-aberto/index.js
+++ desafio-ta-aberto/index.js
@@ -1,12 +1,12 @@
-var texto = document.createElement('p')
-var body = document.querySelector('body')
+var texto = document.createElement("p");
+var body = document.querySelector("body");
 
-function abrir(){   
-   texto.innerText = "Estamos Abertos"   
-   body.append(texto)
+function abrir() {
+  texto.innerText = "Estamos Abertos";
+  body.append(texto);
 }
 
-function fechar(){
-   var tagP = document.querySelector('p')
-   tagP.innerText = "Estamos Fechados"
+function fechar() {
+  var tagP = document.querySelector("p");
+  tagP.innerText = "Estamos Fechados";
 }
