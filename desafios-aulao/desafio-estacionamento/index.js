bb6c070d23e04fb14aa1b50132bf2d0a8265e8e9
Arcenio Souza <arcenio_neto@icloud.com>
2021-12-06 09:24:56 -0300

    Adiciona desafios aulao e qualify 6

---
+++ desafios-aulao/desafio-estacionamento/index.js
@@ -0,0 +1,13 @@
+var estacionamento = {
+   andar1: ['Livre', 'Livre', 'Livre', 'Livre', 'Livre'],
+   andar2: ['Livre', 'Livre', 'Livre', 'Livre', 'Livre'],
+   andar3: ['Livre', 'Livre', 'Livre', 'Livre', 'Livre'],
+   andar4: ['Livre', 'Livre', 'Livre', 'Livre', 'Livre'],
+   andar5: ['Livre', 'Livre', 'Livre', 'Livre', 'Livre'],
+   guardarCarro: function(andar, numVaga){
+      andar = prompt("Digite o número do andar você deseja guardar o carro?")
+      if(andar == '1'){
+         
+      }
+   }
+}
