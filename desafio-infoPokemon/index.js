cbb322531e41b6bb92db507cdca942320bbe7053
Arcenio Souza <arcenio_neto@icloud.com>
2021-12-08 11:20:50 -0300

    Atualiza desafio infoPokemon

--- desafio-infoPokemon/index.js
+++ desafio-infoPokemon/index.js
@@ -1,20 +1,47 @@
-var divPokemon = document.getElementById('infoPoke')
+var divPokemon = document.getElementById("infoPoke");
 
-function pegarPokemon(){
-   var requisicao = new XMLHttpRequest()
-   requisicao.open("GET", "https://pokeapi.co/api/v2/pokemon/35/")
+var divLista = document.getElementById("lista");
 
-   requisicao.onload = function(){
-      if(requisicao.status == 200){
-         var resposta = JSON.parse(requisicao.response)
-         infoPokemonHtml(resposta)
-      } else{
-         console.log("Erro na requisição")
+function pegarPokemon() {
+   var requisicao = new XMLHttpRequest();
+   requisicao.open("GET", "https://pokeapi.co/api/v2/pokemon/35/");
+
+   requisicao.onload = function () {
+      if (requisicao.status == 200) {
+         var resposta = JSON.parse(requisicao.response);
+         infoPokemonHtml(resposta);
+      } else {
+         console.log("Erro na requisição");
       }
-   }
-   requisicao.send()   
+   };
+   requisicao.send();
 }
 
-function infoPokemonHtml(pokemon){
-   divPokemon.innerHTML = `<h1>${pokemon.name}</h1><p>${pokemon.past_types[0].types[0].type.name}</p><img src="${pokemon.sprites.front_shiny}">`
+function gerarListaPokemon() {
+   var requisicao = new XMLHttpRequest();
+   var valorBusca = document.getElementById('busca').value
+   requisicao.open("GET", `https://pokeapi.co/api/v2/pokemon/${valorBusca}/`);
+
+   requisicao.onload = function () {
+      if (requisicao.status == 200) {
+         var resposta = JSON.parse(requisicao.response);
+         infoPokemonHtml(resposta)
+      } else {
+         console.log("Erro na requisição");
+      }
+   };
+
+   requisicao.send();
+}
+
+function infoPokemonHtml(pokemon) {
+   var poke = document.createElement("div");
+
+   poke.setAttribute("class", "cardPoke");
+
+   poke.setAttribute(`${pokemon.types[0].type.name}`, `${pokemon.types[0].type.name}`)
+
+   poke.innerHTML = `<h1>${pokemon.name}</h1><p id="type">${pokemon.types[0].type.name}</p><img src="${pokemon.sprites.front_shiny}">`;
+
+   divLista.appendChild(poke);
 }
