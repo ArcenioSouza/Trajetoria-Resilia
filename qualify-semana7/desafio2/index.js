79c3543301afec5f2c9bab4b73938feb61e594bc
Arcenio Souza <arcenio_neto@icloud.com>
2021-12-13 09:52:31 -0300

    Adiciona qualify 7 ao repositorio

---
+++ qualify-semana7/desafio2/index.js
@@ -0,0 +1,12 @@
+function fabricaDeBolas(tipoBola){
+   tipoBola
+
+   return tipoBola
+}
+
+const jabulani = fabricaDeBolas('bola da copa')
+
+const baba = fabricaDeBolas('bola normal')
+
+console.log(jabulani)
+console.log(baba)
