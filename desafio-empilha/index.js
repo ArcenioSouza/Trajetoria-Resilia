32296e2b90426c9dd5944e1b6488daa145e59d74
Arcenio Souza <arcenio_neto@icloud.com>
2021-12-13 21:12:59 -0300

    Adiciona desafio empilha

---
+++ desafio-empilha/index.js
@@ -0,0 +1,45 @@
+class Pilha {
+   arrayPilha = []
+   top = -1
+
+   empilha(item) {
+      this.top++
+      this.arrayPilha.push(item)
+   }
+
+   desempilha(){
+      try{
+         if(this.top != -1){
+            this.arrayPilha.pop()
+            this.top--
+            return console.log(this.arrayPilha[this.top])
+         } else {
+            throw new Error("Pilha vazia")
+         }         
+      } catch (erro){
+         console.log(erro)
+      }      
+   }
+
+   print(){
+      this.arrayPilha.forEach((item, index) => {
+         console.log(`índice: ${index} item: ${item}`)
+      })
+   }
+}
+
+let pilhaItem = new Pilha
+pilhaItem.empilha("Livro 1")
+pilhaItem.empilha("Livro 2")
+pilhaItem.empilha("Livro 3")
+pilhaItem.empilha("Livro 4")
+
+pilhaItem.desempilha()
+pilhaItem.desempilha()
+pilhaItem.desempilha()
+pilhaItem.desempilha()
+pilhaItem.desempilha()
+pilhaItem.desempilha()
+
+
+pilhaItem.print()
