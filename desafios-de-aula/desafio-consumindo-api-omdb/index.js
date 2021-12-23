364c11bdce94af1e3ca85365bfb3a416b59b9b71
Arcenio Souza <arcenio_neto@icloud.com>
2021-12-23 16:33:51 -0300

    Adiciona responsividade a página home

--- desafios-de-aula/desafio-consumindo-api-omdb/index.js
+++ desafios-de-aula/desafio-consumindo-api-omdb/index.js
@@ -7,6 +7,13 @@ window.addEventListener('load', () => {
    filmesController._criaListaDeFilmes(listaDeFilmes._listaFilmes)
    
    setTimeout(()=>{
-      cards.innerHTML = carousel._templatePoster(listaDeFilmes._listaFilmes)
+      let largura = window.screen.width
+      if(largura > 1000){
+         cards.innerHTML = carousel._templatePosterDesktop(listaDeFilmes._listaFilmes)
+      } else if (largura > 500){
+         cards.innerHTML = carousel._templatePosterTablet(listaDeFilmes._listaFilmes)
+      } else {
+         cards.innerHTML = carousel._templatePosterMobile(listaDeFilmes._listaFilmes)
+      }
    },[1000])   
 })
