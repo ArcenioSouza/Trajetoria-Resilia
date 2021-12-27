8a2d1e5a9fff6ae8303514f53fc6624123fb3e20
Arcenio Souza <arcenio_neto@icloud.com>
2021-12-27 10:38:45 -0300

    Adiciona javascript da pagina de informações

--- desafios-de-aula/desafio-consumindo-api-omdb/index.js
+++ desafios-de-aula/desafio-consumindo-api-omdb/index.js
@@ -1,24 +1,61 @@
-let filmesController = new FilmesController()
-let listaDeFilmes = new ListaFilmes()
-let carousel = new Carousel()
-let cards = document.querySelector('#cards')
+let filmesController = new FilmesController();
+let listaDeFilmes = new ListaFilmes();
+let carousel = new Carousel();
+let filme = new InfoFilme();
+let cards = document.querySelector("#cards");
+let divInformacoes = document.querySelector('.container-informacoes')
+let poster;
+let infoFilmes;
 
-window.addEventListener('load', () => {
-   filmesController._criaListaDeFilmes(listaDeFilmes._listaFilmes)
-   
-   setTimeout(()=>{
-      let largura = window.screen.width
-      let href = window.location.href
+window.addEventListener("load", () => {
+   filmesController._criaListaDeFilmes(listaDeFilmes._listaFilmes);
 
-      if(largura > 1000 && href.indexOf('informacoes') > 0){
-         cards.innerHTML = carousel._templatePosterInformacoes(listaDeFilmes._listaFilmes)
-      } else if(largura > 1000){
-         cards.innerHTML = carousel._templatePosterDesktop(listaDeFilmes._listaFilmes)
-      } else if (largura > 500){
-         cards.innerHTML = carousel._templatePosterTablet(listaDeFilmes._listaFilmes)
+   setTimeout(() => {
+      let largura = window.screen.width;
+      let href = window.location.href;
+
+      if (largura > 1000 && href.indexOf("informacoes") > 0) {
+         cards.innerHTML = carousel._templatePosterInformacoes(
+            listaDeFilmes._listaFilmes
+         );
+         let info = new Informacoes();
+         divInformacoes.innerHTML = info._viewInfoFilme();
+
+      } else if (largura > 1000) {
+         cards.innerHTML = carousel._templatePosterDesktop(
+            listaDeFilmes._listaFilmes
+         );
+      } else if (largura > 500) {
+         cards.innerHTML = carousel._templatePosterTablet(
+            listaDeFilmes._listaFilmes
+         );
       } else {
-         cards.innerHTML = carousel._templatePosterMobile(listaDeFilmes._listaFilmes)
+         cards.innerHTML = carousel._templatePosterMobile(
+            listaDeFilmes._listaFilmes
+         );
       }
-   },[1000])   
-})
 
+      poster = document.querySelectorAll(".poster");
+      
+
+      Array.from(poster).forEach((e) =>
+         e.addEventListener("click", (e) => {
+            e.preventDefault()
+            let href = window.location.href
+            const idFilme = e.target.id
+            filmesController._buscarInformacoes(idFilme)
+
+            if(href.indexOf("informacoes") > 0){
+
+            } else {
+               location = './src/pages/informacoes.html'
+            }
+
+            setTimeout(() => {
+               let info = new Informacoes();
+               divInformacoes.innerHTML = info._viewInfoFilme();
+            }, [1000]);
+         })
+      );
+   }, [1000]);
+});
--- desafios-de-aula/desafio-consumindo-api-omdb/src/controller/FilmesController.js
+++ desafios-de-aula/desafio-consumindo-api-omdb/src/controller/FilmesController.js
@@ -1,6 +1,7 @@
 class FilmesController {
 
    constructor(){
+      this.BdFilme = [],
       this._listaId = ['tt0108778', 'tt0898266', 'tt7556122', 'tt0944947', 'tt5180504', 'tt2661044', 'tt4052886', 'tt11126994', 'tt4145054', 'tt1520211', 'tt2193021', 'tt5420376']
    }
 
@@ -15,5 +16,21 @@ class FilmesController {
             }
          });
       })
+   }
+
+   _buscarInformacoes(id){
+      $.ajax({
+         type: "GET",
+         url: `https://www.omdbapi.com/?i=${id}&apikey=4adad40c`,
+         success: function (response) {  
+
+            let infoFilme = new Filmes(response.imdbID, response.Title, response.Poster, response.Plot, response.Year, response.Runtime, response.Actors, response.Genre)
+
+            localStorage.setItem('infoFilme', JSON.stringify(infoFilme))
+         }
+
+         
+      });
+      
    }
 }
