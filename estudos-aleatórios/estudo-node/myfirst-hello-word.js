2dba4838d73c1c0aaef1e1f1f8a1c33c3b8cf49c
Arcenio Souza <arcenio_neto@icloud.com>
2022-02-14 21:25:18 -0300

    Adiciona hello word com settimeout

--- estudos-aleatórios/estudo-node/myfirst-hello-word.js
+++ estudos-aleatórios/estudo-node/myfirst-hello-word.js
@@ -1,6 +1,7 @@
-var http = require('http');
+const mensagem = (texto) => {
+   console.log(texto)
+}
 
-http.createServer(function (req, res) {
-  res.writeHead(200, {'Content-Type': 'text/html'});
-  res.end('Hello World!');
-}).listen(8080);
+setTimeout(() => {
+   mensagem('hello word')
+}, 5000)
